dd30b1b07c2979a07531550d96a78acd3f15fdc0
LucasCasaril <lucascasaril@hotmail.com>
2022-07-22 21:02:13 -0300

    Feat: Optimizing Motion Equations in Script

--- motion.py
+++ motion.py
@@ -3,7 +3,7 @@ from numpy.linalg import norm
 
 
 def dydt(t,y, G, m1, m2, m3):
-#def dydt(y, t):
+#def dydt(y, t): #For the use of the odeint integrator
 
      #G = 6.67259e-20 #Universal Gravitational Constant (km^3/kg/s^2)
 
@@ -13,47 +13,42 @@ def dydt(t,y, G, m1, m2, m3):
      #m2 = 1e26 # Second Body's Mass - kg
      #m3 = 1e26 # Second Body's Mass - kg
 
-     R1 = [y[0], y[1], y[2]]
-     R2 = [y[3], y[4], y[5]]
-     R3 = [y[6], y[7], y[8]]
+     R1 = y[0:3]
+     R2 = y[3:6]
+     R3 = y[6:9]
 
-     V1 = [y[9],  y[10], y[11]]
-     V2 = [y[12], y[13], y[14]]
-     V3 = [y[15], y[16], y[17]]
+     V1 = y[9:12]
+     V2 = y[12:15]
+     V3 = y[15:18]
 
      r_12 = np.subtract(R2, R1)
-     r_12_vec = list(r_12)
-     r12 = norm(r_12_vec)
- 
+     r12 = norm(r_12)
+
      r_13 = np.subtract(R3, R1)
-     r_13_vec = list(r_13)
-     r13 = norm(r_13_vec)
- 
+     r13 = norm(r_13)
+
      r_23 = np.subtract(R3, R2)
-     r_23_vec = list(r_23)
-     r23 = norm(r_23_vec)
+     r23 = norm(r_23)
 
-     # Finding the acceleration of the Bodies
-     accel_1_X = (G * m2 * ((R2[0] - R1[0])/(r12**3))) + (G * m3 * ((R3[0] - R1[0])/(r13**3)))
-     accel_1_Y = (G * m2 * ((R2[1] - R1[1])/(r12**3))) + (G * m3 * ((R3[1] - R1[1])/(r13**3)))
-     accel_1_Z = (G * m2 * ((R2[2] - R1[2])/(r12**3))) + (G * m3 * ((R3[2] - R1[2])/(r13**3)))
-     
-     accel_1 = [accel_1_X, accel_1_Y, accel_1_Z]
-     
-     accel_2_X = (G * m1 * ((R1[0] - R2[0])/(r12**3))) + (G * m3 * ((R3[0] - R2[0])/(r23**3)))
-     accel_2_Y = (G * m1 * ((R1[1] - R2[1])/(r12**3))) + (G * m3 * ((R3[1] - R2[1])/(r23**3)))
-     accel_2_Z = (G * m1 * ((R1[2] - R2[2])/(r12**3))) + (G * m3 * ((R3[2] - R2[2])/(r23**3)))
-     
-     accel_2 = [accel_2_X, accel_2_Y, accel_2_Z]
-     
-     accel_3_X = (G * m1 * ((R1[0] - R3[0])/(r13**3))) + (G * m2 * ((R2[0] - R3[0])/(r23**3)))
-     accel_3_Y = (G * m1 * ((R1[1] - R3[1])/(r13**3))) + (G * m2 * ((R2[1] - R3[1])/(r23**3)))
-     accel_3_Z = (G * m1 * ((R1[2] - R3[2])/(r13**3))) + (G * m2 * ((R2[2] - R3[2])/(r23**3)))
-     
-     accel_3 = [accel_3_X, accel_3_Y, accel_3_Z]
+     # Creating the return motion vector dydt = [V1 V2 V3 A1 A2 A3]
+     dydt = np.zeros(18)
 
+     dydt[0:3] = V1
+     dydt[3:6] = V2
+     dydt[6:9] = V3
 
-     dydt = np.concatenate((V1, V2, V3, accel_1, accel_2, accel_3), axis=None)
+     # Finding the Accelerations thought the Motion Equation     
+     dydt[9:12]  = [(G * m2 * ((R2[0] - R1[0])/(r12**3))) + (G * m3 * ((R3[0] - R1[0])/(r13**3))),
+                    (G * m2 * ((R2[1] - R1[1])/(r12**3))) + (G * m3 * ((R3[1] - R1[1])/(r13**3))),
+                    (G * m2 * ((R2[2] - R1[2])/(r12**3))) + (G * m3 * ((R3[2] - R1[2])/(r13**3)))]
+
+     dydt[12:15] = [(G * m1 * ((R1[0] - R2[0])/(r12**3))) + (G * m3 * ((R3[0] - R2[0])/(r23**3))),
+                    (G * m1 * ((R1[1] - R2[1])/(r12**3))) + (G * m3 * ((R3[1] - R2[1])/(r23**3))),
+                    (G * m1 * ((R1[2] - R2[2])/(r12**3))) + (G * m3 * ((R3[2] - R2[2])/(r23**3)))]
+
+     dydt[15:18] = [(G * m1 * ((R1[0] - R3[0])/(r13**3))) + (G * m2 * ((R2[0] - R3[0])/(r23**3))),
+                    (G * m1 * ((R1[1] - R3[1])/(r13**3))) + (G * m2 * ((R2[1] - R3[1])/(r23**3))),
+                    (G * m1 * ((R1[2] - R3[2])/(r13**3))) + (G * m2 * ((R2[2] - R3[2])/(r23**3)))]
 
      # Returning the vector with Velocity and Acceleration of the Bodies 
      return dydt
